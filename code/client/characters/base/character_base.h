950db6a3f4d0a5cb728bcec44bf41fc0b0f33f61
Thomas <tpreisner@gmail.com>
2026-02-02 05:38:18 -0500

    initial add of the character base class

---
+++ code/client/characters/base/character_base.h
@@ -0,0 +1,14 @@
+/********************************************************************/
+/*                      Copyright 2026                              */
+/*           Created and Maintained by Thomas Preisner              */
+/********************************************************************/
+#pragma once
+//  The CharacterBase class exist to provide a common base interface 
+//      for the enemies, player characters, and non-player characters.
+
+
+class CharacterBase {
+
+private:
+
+};
